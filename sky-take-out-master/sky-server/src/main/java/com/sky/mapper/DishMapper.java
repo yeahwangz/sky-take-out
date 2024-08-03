cdde5a19a13c9592b6c503761261472147c3be52
yeahwangz <abc494823@gmail.com>
2024-08-03 22:29:52 +0800

    新增菜品业务代码开发

---
+++ sky-take-out-master/sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -0,0 +1,28 @@
+package com.sky.mapper;
+
+import com.sky.annotation.AutoFill;
+import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface DishMapper {
+
+    /**
+     * 根据分类id查询菜品数量
+     * @param categoryId
+     * @return
+     */
+    @Select("select count(id) from sky_take_out.dish where category_id = #{categoryId}")
+    Integer countByCategoryId(Long categoryId);
+
+    /**
+     * 插入菜品数据
+     * @param dish
+     */
+    @AutoFill(value = OperationType.INSERT)
+    void insert(Dish dish);
+
+
+}
